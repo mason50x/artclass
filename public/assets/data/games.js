d8732a0006627552122f9846b1dcd54620809a6f
mason50x <masonsyzn@gmail.com>
2025-09-15 18:49:50 -0500

    Add descriptions to various games in games.js for enhanced user engagement and clarity.

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -2,132 +2,147 @@ var games = [
   {
     id: 'idlebreakout',
     title: 'Idle Breakout',
+    description: 'click balls, break bricks, become unstoppable brick-smashing machine',
     url: 'https://assets.3kh0.net/idle-breakout/index.html/',
     image: 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
   },
   {
     id: 'eggycar',
     title: 'Eggy Car',
+    description: 'drive car with egg, don\'t drop egg, egg more important than life',
     url: 'https://hva2021.github.io/eggy-car/index.html',
     image: 'https://assets.3kh0.net/eggycar/eggy-car.png'
   },
   {
     id: 'rocketleague',
     title: 'Rocket League',
+    description: 'cars playing soccer but with rockets and way too much boost',
     url: 'https://glcdn.githack.com/kaioxdev/legacy-assets/-/raw/main/Rocket-League/index.html',
     image: 'https://www.softgozar.com/Image/Softwares/Thumbnail/Rocket_League_Anniversary_SoftGozar.com.png'
   },
   {
     id: 'cookieclicker',
     title: 'Cookie Clicker',
-    description: 'Cookie Clicker is an addictive incremental game where players click to produce cookies, unlock upgrades, and aim to amass an ever-growing mountain of delicious digital treats.',
+    description: 'click cookie, buy grandmas, ascend to cookie godhood, question life choices',
     url: 'https://sushi8756.github.io/Cookie-Clicker-2.031/',
     image: 'https://cdn.discordapp.com/icons/339131488111034389/73f15cd93d96504af9877a53816d110c.png'
   },
   {
     id: 'jacksmith',
     title: 'Jacksmith',
+    description: 'donkey blacksmith makes weapons, fights things, surprisingly deep crafting',
     url: 'https://art-class.github.io/assets/jacksmith',
     image: 'https://lh3.googleusercontent.com/dJAftLFmxmWdTAEjuolFrtNNAX3D0HjODTm9703CP7JqM_jFgOgPR75d6xqRsYTwoTot'
   },
   {
     id: 'thereisnogame',
     title: 'There Is No Game',
+    description: 'narrator insists there\'s no game, you prove them wrong, meta weirdness',
     url: 'https://assets.3kh0.net/there-is-no-game/index.html',
     image: 'https://play-lh.googleusercontent.com/a6DfrYUL6viV0LAZFplqqA44gE0LHE0PyDnKHgdlydiUvccwzSBWrmRQSx5cjdL74Us=w240-h480-rw'
   },
   {
     id: 'fbwg',
     title: 'Fireboy and Watergirl',
-    description: 'A 2 player game where you have to work together using logic and teamwork to get to the finish line.',
+    description: 'fire boy and water girl solve puzzles, friendship stronger than physics',
     url: 'https://fireboy-and-watergirl-online.github.io/file/',
     image: 'https://play-lh.googleusercontent.com/5-YcjiTyMrPJEZe078dHMxnCxcg2A_8NWJZQieRvn2Gkf8JOuHZEbTmFyQ9gn5p2lnU'
   },
   {
     id: 'driftboss',
     title: 'Drift Boss',
+    description: 'drift forever or die trying, one button controls your destiny',
     image: 'https://play-lh.googleusercontent.com/fLrnXuD1B0w6FEDJ1UKmv_TYbNyJ4GwzsQu9ltx57MfIj037nig4cVunG3CYmczkjMDA',
     url: 'https://driftbossonline.github.io/file/'
   },
   {
     id: 'osu',
     title: 'osu!',
-    description: 'A web version of osu!',
+    description: 'click circles to the beat, become rhythm game god, break mouse',
     url: 'https://web-osu.github.io/',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Osu%21_Logo_2016.svg/1200px-Osu%21_Logo_2016.svg.png',
   },
   {
     id: 'cuttherope',
     title: 'Cut the Rope',
+    description: 'feed candy to green monster, physics puzzles, om nom nom',
     url: 'https://script.google.com/macros/s/AKfycbzC5BJN124UUKkQqKJwi6D_xOIz7Vy0fHyc1Fl8NEnSUtRXD-AUGnkKt3u1bugFLT5s/exec',
     image: 'https://play-lh.googleusercontent.com/8FNcAyLXtQB_0Ux2ZO8VZoEoihL6a5VMBNf6V2lydRM24hXLnNUdlEup1d5miVjl3JY'
   },
   {
     id: 'tombofthemask',
     title: 'Tomb of the Mask',
+    description: 'climb walls wearing magic mask, avoid spikes, die repeatedly',
     url: 'https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/sk1bx/element@9cccb2fafb569c963501a5d818472b561121cea6/totm.xml',
     image: 'https://play-lh.googleusercontent.com/gTd127I81O2i2Q6kfCJoN-M0OSWmgsdjB47orUFpLwANW7VZLJYrOxMVt-OpilDXszg=w240-h480-rw'
   },
   {
     id: 'backrooms',
     title: 'Backrooms',
-    description: 'Scary game based on the viral 4Chan post.',
+    description: 'infinite yellow rooms, fluorescent lights, something watching you',
     url: 'https://backroomsgame.io/game/backrooms/',
     image: 'https://media.wired.com/photos/627b0c1fb6048c47d506c6c0/1:1/w_679,h_679,c_limit/Backrooms-Games.jpg'
   },
   {
     id: 'youarebezos',
     title: 'You are Bezos',
+    description: 'spend billionaire money faster than you earn it, impossible challenge',
     url: 'https://assets.3kh0.net/you-are-bezos/index.html',
     image: 'https://img.itch.zone/aW1nLzE1NzI2NjIucG5n/original/Equhiy.png'
   },
   {
     id: 'papaburgers',
     title: 'Papa\'s Burgeria',
+    description: 'flip burgers, serve customers, become fast food overlord',
     url: 'https://mazahacka2017.github.io/papas-burgeria',
     image: 'https://i.flipline.com/gamefiles/papasburgeriatogo/app_upsell_C.jpg'
   },
   {
     id: 'papasicecream',
     title: 'Papa\'s Scooperia',
+    description: 'scoop ice cream, add toppings, satisfy sugar addicts',
     url: 'https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/1ee20621-61bc-4ec8-a8ec-5e839c2e6edc%2Fpapa-s-scooperia.xml',
     image: 'https://play-lh.googleusercontent.com/KpLBHo89bWG3jmuYb_KqDD1rZSY4XzXcHiMLZ1PCv83GMwVMcJluuuXrT32oQGCo51A'
   },
   {
     id: 'ultimatechess',
     title: 'Ultimate Chess',
+    description: 'chess but with lasers and explosions, somehow still chess',
     url: 'https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://s3.amazonaws.com/production-assetsbucket-8ljvyr1xczmb/1ee20621-61bc-4ec8-a8ec-5e839c2e6edc%2Fultimate-chess.xml',
     image: 'https://img.kbhgames.com/2010/05/Ultimate-Chess.jpg'
   },
   {
     id: 'jetpackjoyride',
     title: 'Jetpack Joyride',
+    description: 'fly jetpack, dodge missiles, collect coins, crash spectacularly',
     url: 'https://assets.3kh0.net/jetpack-joyride/index.html',
     image: 'https://play-lh.googleusercontent.com/Pl2nmu5U9CH3NQqLBTjnokoV325zESrX6KIaHrwSqREEJDWIv1yJpRCXzl9r3oMnEWHC',
   },
   {
     id: 'shellshockers',
     title: 'Shell Shockers',
-    description: 'Shell Shockers is unique online shooter in first-person perspective where all the characters are represented exclusively by eggs.',
+    description: 'eggs with guns shoot other eggs, yolk everywhere, surprisingly violent',
     url: 'https://assets.3kh0.net/shellshockers/index.html',
     image: 'https://play-lh.googleusercontent.com/P-nde227L29s8w5U44kTPLiEnMEJUhJpEr4jL6tD6LV65Xz0JZtI4wEyFN-smsNrx-Q'
   },
   {
     id: 'plantsvszombies',
     title: 'Plants Versus Zombies 1.0',
-    description: 'Plants Versus Zombies 1.0 is a tower defense video game where players strategically plant a variety of plants to fend off waves of zombie invaders.',
+    description: 'plants shoot zombies, zombies eat brains, your lawn becomes battlefield',
     url: 'https://glcdn.githack.com/kaioxdev/legacy-assets/-/raw/main/plants%20vs%20zombies%201/plants%20vs%20zombies.html',
     image: 'http://cm1.narvii.com/6715/5bef9754b89ffbf36e5afb2c4dac9860b9614987_00.jpg'
   },
   {
     id: 'happywheels',
     title: 'Happy Wheels',
+    description: 'wheelchair guy races through death traps, nothing happy about it',
     url: 'https://script.google.com/macros/s/AKfycbyfMPVIGx6dJPrYKeE9e4Erj949-dH28pWVRjdV1vgnoylpBV8af03JNLoz2MwAIBLECg/exec',
     image: 'https://play-lh.googleusercontent.com/SV8RsV5udSeeONjatT5SwleP6lzV6PjtNPs2VvyohJXWSG9fFLNOfslDEHbpDN337wQ'
   },
   {
     id: 'motox3m',
     title: 'MotoX3M',
+    description: 'motorcycle stunts through death courses, physics hate you',
     //url: 'https://assets.3kh0.net/motox3m/index.html',
     url: 'https://art-class.github.io/assets/motox3m',
     image: 'https://play-lh.googleusercontent.com/AbQMuT4ncBfWDqNF-4ubmfaRag59nrW3fJARDsHK51_PENScmWCnMFeQX81wfPlfGBo'
@@ -135,31 +150,35 @@ var games = [
   {
     id: 'thefinalearth',
     title: 'The Final Earth',
-    description: 'Earth has been destroyed. It\'s a good thing you teleported to a new planet, also known as The Final Earth. Gather resources and build a colony',
+    description: 'earth exploded, build new civilization from scratch, no pressure',
     url: 'https://assets.3kh0.net/the-final-earth/index.html',
     image: 'https://img.itch.zone/aW1nLzIyMTU1NDAucG5n/original/6vGlZe.png'
   },
   {
     id: 'fnf',
     title: 'Friday Night Funkin',
+    description: 'rap battle to win girlfriend, beep boop your way to love',
     url: 'https://fridaynightfunkinweek7.github.io/',
     image: 'https://img.utdstc.com/icon/807/216/8072161110b1f6a2990a4baeb55eaaeab761fc199d9c001c7b6623f4cf8c6eb7:200'
   },
   {
     id: 'paperio2',
     title: 'Paper.io 2',
+    description: 'draw lines, claim territory, cut enemies while they cut you',
     url: 'https://assets.3kh0.net/paperio2/index.html',
     image: 'https://play-lh.googleusercontent.com/KxcjUrs8S75iOPnKBR2b-s7eGtqbREwLgzttVxaM9Znl5ZxsDCl6qEZHny4rBkMeYA=w240-h480-rw'
   },
   {
     id: 'impossiblequiz',
     title: 'The Impossible Quiz',
+    description: 'quiz with impossible questions, logic not included, rage guaranteed',
     url: 'https://proudparrot2.github.io/msg-archive/g_mes/impossiblequiz1/impossiblequiz1/index.html',
     image: 'https://ih1.redbubble.net/image.489460698.9522/st,small,507x507-pad,600x600,f8f8f8.jpg'
   },
   {
     id: 'ducklife4',
     title: 'Duck Life 4',
+    description: 'train duck to be athletic champion, quack your way to victory',
     url: 'https://htmlxm.github.io/h/duck-life-4/',
     image: 'https://play-lh.googleusercontent.com/AUGkRrKMtUCLVE9vCvlfMY9Ny9EBqzx17yejVtEEhvpkw-H6lJlvBHgCMqPJm8HV_tM'
   },
